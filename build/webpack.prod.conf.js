9116f9bc48aa182409b878545414771b6c2c1b19
石浩 <1281875047@qq.com>
2018-06-20 16:24:39 +0800

    ..

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,6 +11,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : require('../config/prod.env')
