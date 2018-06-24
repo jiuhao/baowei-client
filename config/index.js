f0b55ae45456c17695269cab8030c67fb8724cc4
石浩 <1281875047@qq.com>
2018-06-24 15:23:57 +0800

    ..

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'client/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/client',
 
     /**
      * Source Maps
